e3b2a38d537b4d8264af57161e79997e14a199df
Kritchanaxt <145214064+Kritchanaxt@users.noreply.github.com>
2023-11-11 00:17:29 +0700

    Update Page1
    Add dropdown in js, css, image
    logo apple and googleplay

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 
-/*nav transparent
+//dropdown
+$(document).ready(function(){
+  $('.languageSection .dropdown-toggle').click(function(){
+      $('.languageSection .dropdown-menu').toggle();
+  });
+
+  $('.languageSection .dropdown-menu a1').click(function(){
+      var selectedLanguage = $(this).attr('language');
+      // ทำตามต้องการเมื่อเลือกภาษา
+      console.log('Selected language:', selectedLanguage);
+  });
+});
+
+
+//navbar transparent
 window.addEventListener('scroll', () => {
-  const nav = document.getElementById('nav');
+  const nav = document.getElementById('navb');
   if (window.scrollY > 0) {
     nav.classList.add('scroll');
   } else {
     nav.classList.remove('scroll');
   }
-});*/
+});
