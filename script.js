22c2a30e7bd20e32b32a5d7accbe0c9bc61a6dea
Kritchanaxt <145214064+Kritchanaxt@users.noreply.github.com>
2023-11-11 23:10:08 +0700

    Page 1
    update funtion css, js, dropdown, animetion, and navbar

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-
-//dropdown
+// Dropdown!!
 $(document).ready(function(){
+  // เมื่อคลิกที่ dropdown-toggle ใน languageSection
   $('.languageSection .dropdown-toggle').click(function(){
+      // สลับการแสดง/ซ่อน dropdown-menu
       $('.languageSection .dropdown-menu').toggle();
   });
 
+  // เมื่อคลิกที่องค์ประกอบ a1 ภายใน dropdown-menu ของ languageSection
   $('.languageSection .dropdown-menu a1').click(function(){
+      // ดึงค่า attribute 'language' จากองค์ประกอบ a1 ที่ถูกคลิก
       var selectedLanguage = $(this).attr('language');
-      // ทำตามต้องการเมื่อเลือกภาษา
       console.log('Selected language:', selectedLanguage);
   });
 });
 
 
-//navbar transparent
-window.addEventListener('scroll', () => {
-  const nav = document.getElementById('navb');
-  if (window.scrollY > 0) {
-    nav.classList.add('scroll');
-  } else {
-    nav.classList.remove('scroll');
-  }
-});
+
+// Navbar transparent!!
+var nav = document.getElementById("navbar"); 
+// รับอิลิเมนต์ nav
+
+var sticky = nav.offsetTop; 
+// รับตำแหน่งออฟเซตของ nav
+
+function handleScroll() {
+    if (window.pageYOffset > sticky) {
+        nav.classList.add("solid");
+    } else {
+        nav.classList.remove("solid");
+    }
+}
+// ฟังก์ชันเพื่อเพิ่มหรือลบคลาส "solid" ขึ้นอยู่กับตำแหน่งการเลื่อน
+
+window.addEventListener("scroll", handleScroll);
+// ผูกฟังก์ชัน handleScroll กับเหตุการณ์การเลื่อน
+
