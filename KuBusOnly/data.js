62963d9a8f909f5bef822c0ff1d1174dba3a3581
Kritchanaxt <145214064+Kritchanaxt@users.noreply.github.com>
2023-12-29 20:51:35 +0700

    Update data.js

---
+++ KuBusOnly/data.js
@@ -0,0 +1,92 @@
+const data = {
+    "eng" : 
+    {
+        //navbar
+        "nav-link1": "3D VIEW",
+        "nav-link2": "Contact Us",
+        "nav-link dropdown-toggle": "English1",
+
+        //Page1
+        "title1": "Making Transit",
+        "auto-type": "",
+        "title2": "KU BUS  Real-time Transit App",
+
+        //Page2
+        "sub-title": "Your Travel Companion",
+        "Text-Routes1": "Search Routes",
+        "Text-Routes2": "Find transit stops and routes for",
+        "Text-Routes3": "Kasetsart University",
+        "Text-TrackLive1": "Track Live Buses",
+        "Text-TrackLive2": "See real-time bus locations",
+        "Text-TrackLive3": "approaching your stop",
+        "Text-GetDirections1": "Get Directions",
+        "Text-GetDirections2": "Get recommended routes to",
+        "Text-GetDirections3": "travel from A to B",
+
+        //Page3
+        "sub-title3D": " SEE THE BUS IN 3D",
+        "text-explain1": "If you want to see the bus in  <br> style 3D animated video, </br>press  the “ Open 3D ” button.",
+        "text-explain2": "You can enjoy watching <br>animations and details </br>of this 3D model.",
+
+        //Page4
+        "Text-Station1": "Are You a Transit <br> Operator?",
+        "Text-Station2": "Join the digital transportation   <br> platform of KU BUS <br>to access services broadly <br> and for users to follow their <br> services in real time ",
+        "Text-join": "Join KU BUS Now!",
+
+        //Page5
+        "fivetop": "KU BUS began with a small team of aims to make public transportation. <br> A simple, comfortable, and seamless experience for all, free of charge. <br> Every penny supported will go into the development of <br> KU BUS for a better university experience.",
+        "fivesub": "Support KU BUS",
+        "playtop": "GET IT ON",
+        "appletop": "GET IT ON",
+        "bothome": "Home",
+        "botsup": "Support KU BUS",
+        "botjoin": "Join Our Platform",
+        "botfqa": "FQA",
+        "botcontact": "Contact",
+    },
+    "thai" :
+    [
+        //navbar
+        "nav-link1": "มุมมอง 3 มิติ",
+        "nav-link2": "ติดต่อเรา",
+        "nav-link dropdown-toggle": "English",
+
+        //Page1
+        "title1": "Making Transit",
+        "auto-type": "",
+        "title2": "KU BUS  Real-time Transit App",
+
+        //Page2
+        "sub-title": "Your Travel Companion",
+        "Text-Routes1": "Search Routes",
+        "Text-Routes2": "Find transit stops and routes for",
+        "Text-Routes3": "Kasetsart University",
+        "Text-TrackLive1": "Track Live Buses",
+        "Text-TrackLive2": "See real-time bus locations",
+        "Text-TrackLive3": "approaching your stop",
+        "Text-GetDirections1": "Get Directions",
+        "Text-GetDirections2": "Get recommended routes to",
+        "Text-GetDirections3": "travel from A to B",
+
+        //Page3
+        "sub-title3D": " SEE THE BUS IN 3D",
+        "text-explain1": "If you want to see the bus in  <br> style 3D animated video, </br>press  the “ Open 3D ” button.",
+        "text-explain2": "You can enjoy watching <br>animations and details </br>of this 3D model.",
+
+        //Page4
+        "Text-Station1": "Are You a Transit <br> Operator?",
+        "Text-Station2": "Join the digital transportation   <br> platform of KU BUS <br>to access services broadly <br> and for users to follow their <br> services in real time ",
+        "Text-join": "Join KU BUS Now!",
+
+        //Page5
+        "fivetop": "KU BUS began with a small team of aims to make public transportation. <br> A simple, comfortable, and seamless experience for all, free of charge. <br> Every penny supported will go into the development of <br> KU BUS for a better university experience.",
+        "fivesub": "Support KU BUS",
+        "playtop": "GET IT ON",
+        "appletop": "GET IT ON",
+        "bothome": "Home",
+        "botsup": "Support KU BUS",
+        "botjoin": "Join Our Platform",
+        "botfqa": "FQA",
+        "botcontact": "Contact",
+    ]
+}
