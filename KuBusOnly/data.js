194469d58e5d5f371d8f2f8f793c0ff4fc4c0702
Kritchanaxt <145214064+Kritchanaxt@users.noreply.github.com>
2023-12-30 16:07:35 +0700

    update data.js

--- KuBusOnly/data.js
+++ KuBusOnly/data.js
@@ -45,36 +45,36 @@ const data = {
         "botcontact": "Contact",
     },
     "thai" :
-    [
+    {
         //navbar
         "nav-link1": "มุมมอง 3 มิติ",
         "nav-link2": "ติดต่อเรา",
-        "nav-link dropdown-toggle": "English",
+        "nav-link dropdown-toggle": "ภาษาไทย",
 
         //Page1
-        "title1": "Making Transit",
+        "title1": "เดินทางด้วยความสะดวก",
         "auto-type": "",
-        "title2": "KU BUS  Real-time Transit App",
+        "title2": "KU BUS  แอปติดตามและนำทางขนส่งแบบเรียลไทม์",
 
         //Page2
-        "sub-title": "Your Travel Companion",
-        "Text-Routes1": "Search Routes",
-        "Text-Routes2": "Find transit stops and routes for",
-        "Text-Routes3": "Kasetsart University",
-        "Text-TrackLive1": "Track Live Buses",
-        "Text-TrackLive2": "See real-time bus locations",
-        "Text-TrackLive3": "approaching your stop",
-        "Text-GetDirections1": "Get Directions",
-        "Text-GetDirections2": "Get recommended routes to",
-        "Text-GetDirections3": "travel from A to B",
+        "sub-title": "เพื่อนร่วมเดินทางของคุณ",
+        "Text-Routes1": "ค้นหาเส้นทาง",
+        "Text-Routes2": "ค้นหาจุดจอดและเส้นทางสำหรับ",
+        "Text-Routes3": "มหาวิทยาลัยเกษตรศาสตร",
+        "Text-TrackLive1": "ติดตามรถโดยสาร",
+        "Text-TrackLive2": "ดูตำแหน่งรถบัสแบบเรียลไทม์ที่ใกล้ถึงจุด",
+        "Text-TrackLive3": "จอดของคุณ",
+        "Text-GetDirections1": "แนะนำเส้นทาง",
+        "Text-GetDirections2": "ไม่รู้ว่าต้องตึกเรียนอะไร? KU BUS ",
+        "Text-GetDirections3": "ก็ช่วยแนะนำเส้นทางให้คุณได้",
 
         //Page3
-        "sub-title3D": " SEE THE BUS IN 3D",
-        "text-explain1": "If you want to see the bus in  <br> style 3D animated video, </br>press  the “ Open 3D ” button.",
-        "text-explain2": "You can enjoy watching <br>animations and details </br>of this 3D model.",
+        "sub-title3D": "ดูรถบัสในแบบ 3 มิติ",
+        "text-explain1": "Iหากคุณต้องการดูรถบัสในรูปแบบ  <br> วิดิโอภาพเคลื่อนไหว 3 มิติ , </br>ให้กดปุ่ม “ Open 3D ”.",
+        "text-explain2": "คุณสามารถเพลิดเพลินกับการชม <br>ภาพเคลื่อนไหวและรายละเอียด </br>ของโมเดล 3 มิตินี้ได้.",
 
         //Page4
-        "Text-Station1": "Are You a Transit <br> Operator?",
+        "Text-Station1": "คุณเป็นผู้ใช้บริการรถโดยสาร?",
         "Text-Station2": "Join the digital transportation   <br> platform of KU BUS <br>to access services broadly <br> and for users to follow their <br> services in real time ",
         "Text-join": "Join KU BUS Now!",
 
@@ -88,5 +88,6 @@ const data = {
         "botjoin": "Join Our Platform",
         "botfqa": "FQA",
         "botcontact": "Contact",
-    ]
+    
+    }
 }
